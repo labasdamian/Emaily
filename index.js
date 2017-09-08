f2c31622cb76cf9ba00a34462c14d84f28d53da5
Damian <labasdamian@gmail.com>
2017-09-08 22:51:52 +0200

    add connection to google with google oAuth

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys.js");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "hello" });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID:
+        "674768788324-nj3qg4lie4mnoqg2huo4cjrc83sdmq02.apps.googleusercontent.com",
+      clientSecret: "qgirIeK-DyvAmbREd2nBtpeM",
+      callbackURL: "/auth/google/callback"
+    },
+    accesToken => {
+      console.log("accesToken: ", accesToken);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
+
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 const PORT = process.env.PORT || 5000;
 
