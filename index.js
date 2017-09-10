980e8e7b3f2fd4b0fb9a630541148581961ea50d
Damian <labasdamian@gmail.com>
2017-09-10 19:33:04 +0200

    install mongoose and create user model

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
 require("./services/passport");
+require("./models/User");
 
 const app = express();
+
+mongoose.connect(keys.mongoURI);
 
 require("./routes/authRoutes")(app);
 
