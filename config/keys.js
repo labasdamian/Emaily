9b71b0dfd3df225e95f1d9a8403497b468dd6d7a
Damian <labasdamian@gmail.com>
2017-12-03 20:29:04 +0100

    production env

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+
+if(procces.env.NODE_ENV === 'production') {
+	module.exports = require('./prod');
+} else {
+	module.exports = require('./dev');
+}
---
+++ config/prod.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+	clientID: proccess.env.GOOGLE_CLIENT_ID,
+	clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+	mongoURI: process.env.MONGO_URI,
+	cookieKey: process.env.COOKIE_KEY
+};
