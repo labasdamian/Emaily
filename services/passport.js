4813378840062b79eb656e861a5126b1916e1346
Damian <labasdamian@gmail.com>
2017-12-03 20:53:27 +0100

    fix env

--- services/passport.js
+++ services/passport.js
@@ -20,7 +20,8 @@ passport.use(
     {
       clientID: keys.clientID,
       clientSecret: keys.clientSecret,
-      callbackURL: "/auth/google/callback"
+      callbackURL: "/auth/google/callback",
+			proxy: true
     },
     (accesToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then(existingUser => {
